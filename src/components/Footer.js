35f403b34699bf4f43c29ae8b3abb36d57ff89a8
Leonel <leoneloliver@gmail.com>
2021-06-18 09:04:17 -0400

    UI improvements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { FaHeart } from 'react-icons/fa';
+import { SiBuymeacoffee } from 'react-icons/si';
 
 function Footer() {
-  const currentYear = new Date().getFullYear();
+  // const currentYear = new Date().getFullYear();
   return (
-    <footer>
-      <p>Coppyright © {currentYear} | Leonel Oliveira</p>
+    <footer className="shadow-sm">
+      <p>Made with <SiBuymeacoffee className="coffee"/> + <FaHeart className="heart"/> by <a href="https://leo-frontend.netlify.app/" rel="noopener noreferrer" target="_blank">Leonel Oliveira</a></p>
     </footer>
   );
 }
